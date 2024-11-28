5efa923640988003f39f270e8120bd8e3d20d2f2
priya2208VP <priyavenkat2208@gmail.com>
2024-11-28 21:53:55 +0530

    First commit priya

---
+++ src/FirstProgram.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class FirstProgram
+{
+	
+public static void main(String args[])
+{
+	int a,b,s,p;
+	Scanner Scan = new Scanner(System.in);
+	a=Scan.nextInt();
+	b=Scan.nextInt();
+	s=a+b;
+	p=a*b;
+	System.out.println("sum="+s);
+	System.out.println("product="+p);			
+}
+}
